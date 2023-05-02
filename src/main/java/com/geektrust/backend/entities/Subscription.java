e0b2c1fe1fc361aaa903cb2055e2d47effcbb41f
Vaibhav Kale <64345541+vaiibhavkale@users.noreply.github.com>
2023-05-02 17:30:16 +0530

    Update Subscription.java

--- src/main/java/com/geektrust/backend/entities/Subscription.java
+++ src/main/java/com/geektrust/backend/entities/Subscription.java
@@ -9,4 +9,34 @@ public abstract class Subscription {
     public Integer getPrice() {
         return price;
     }
+    
+    public Subscription(int validityInMonths, int price, int getValidityInMonths){
+        this.validityInMonths = validityInMonths;
+        this.price = price;
+        this.getValidityInMonths = validityInMonths;
+    }
+    
+    public void int getValidityInMonths(){
+        return validityInMonths;
+    }
+    
+    public int setValidityInMonths(int validityInMonths){
+        this.validityInMoths = validityInMonths;
+    }
+    
+     public void int getPrice(){
+        return price;
+    }
+    
+    public int setPrice(int price){
+        this.price = price;
+    }
+    
+    public void int getValidityInMonths(){
+        return getValidityInMonths;
+    }
+
+    public int setValidityInMonths(int getValidityInMonths){
+        this.getValidityInMonths = getValidityInMonths;
+    }
 }
